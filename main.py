5935256fc01f340bd93ef80f8269b9119740288a
sabre <djsoundnr1@gmail.com>
2019-05-29 16:51:22 +0200

    started doing menu

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
-import sqlite3
-
-filename = '/database/company.db'
 
 
 def start_init():
@@ -11,19 +8,32 @@ def start_init():
     except FileNotFoundError:
         print("Database does not exist. Check if the file exists and is in proper directory.")
         print("Enter 'y' if you want to create a new database, enter any other key if you want to abort.")
-        if input(">>>").lower() == 'y':
+        if input(">>> ").lower() == 'y':
             open(filename).close()
             print("Database has been created.")
-
+    finally:
+        print("Database is ready to use.")
 
 
 def main():
     start_init()
-
+    main_menu()
 
 if __name__ == "__main__":
+    filename = '/database/company.db'
     main()
 
 
 def main_menu():
-    print()
+    input_dict = {1: "worker ", }
+    while True:
+        print_main_menu()
+        choice = input(">>> ")
+        if choice in input_dict:
+            break
+        else:
+            print("Invalid selection. Try again. \n")
+
+
+def print_main_menu():
+    print("1 - Worker management")
